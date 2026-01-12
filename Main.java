9b5357857581aefa4e52e9e8173cb7acae9e0c1a
gulamekhwaja1999 <gulamekhwaja5@gmail.com>
2026-01-12 22:33:53 +0530

    Add Main.java Java program

---
+++ Main.java
@@ -0,0 +1,6 @@
+class Main{
+    public static void main(String[] args) {
+        System.err.println("hello World ");
+        System.err.println("This is my first jenkins project testing");
+    }
+}
